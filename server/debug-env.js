09d7f964c675d6fee538ad7b06a66ba56a1ef958
amit <akamit169@gmail.com>
2025-08-27 21:16:48 +0530

    test

--- server/debug-env.js
+++ server/debug-env.js
@@ -1,11 +1,5 @@
-export default async function handler(req, res) {
-  res.json({
-    NODE_ENV: process.env.NODE_ENV,
-    USE_MOCK_DB: process.env.USE_MOCK_DB,
-    DB_HOST: process.env.DB_HOST,
-    DB_USER: process.env.DB_USER,
-    DB_NAME: process.env.DB_NAME,
-    DB_PORT: process.env.DB_PORT,
-    FRONTEND_URL: process.env.FRONTEND_URL,
+export default function handler(req, res) {
+  res.status(200).json({
+    env: process.env
   });
 }
